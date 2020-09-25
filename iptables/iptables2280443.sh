06e87d26631343a2c05be7698c5a6d98d72a7d32
MikewuTV <49505963+mikewubox@users.noreply.github.com>
2020-09-25 19:14:33 +0800

    Update iptables2280443.sh

--- iptables/iptables2280443.sh
+++ iptables/iptables2280443.sh
@@ -2,25 +2,25 @@
 #
 #
 # Script is for stoping Portscan and smurf attack
-
+#
 ### first flush all the iptables Rules
 iptables -F
-
-
+#
+#
 # INPUT iptables Rules
 # Accept loopback input
 iptables -A INPUT -i lo -p all -j ACCEPT
-
+#
 # allow 3 way handshake
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
+#
 ### DROPspoofing packets
 iptables -A INPUT -s 10.0.0.0/8 -j DROP 
 iptables -A INPUT -s 169.254.0.0/16 -j DROP
 iptables -A INPUT -s 172.16.0.0/12 -j DROP
 iptables -A INPUT -s 127.0.0.0/8 -j DROP
 iptables -A INPUT -s 192.168.0.0/24 -j DROP
-
+#
 iptables -A INPUT -s 224.0.0.0/4 -j DROP
 iptables -A INPUT -d 224.0.0.0/4 -j DROP
 iptables -A INPUT -s 240.0.0.0/5 -j DROP
@@ -29,57 +29,57 @@ iptables -A INPUT -s 0.0.0.0/8 -j DROP
 iptables -A INPUT -d 0.0.0.0/8 -j DROP
 iptables -A INPUT -d 239.255.255.0/24 -j DROP
 iptables -A INPUT -d 255.255.255.255 -j DROP
-
+#
 #for SMURF attack protection
 iptables -A INPUT -p icmp -m icmp --icmp-type address-mask-request -j DROP
 iptables -A INPUT -p icmp -m icmp --icmp-type timestamp-request -j DROP
 #iptables -A INPUT -p icmp -m icmp -m limit --limit 1/second --limit-burst 2 -j ACCEPT
-
+#
 # Droping all invalid packets
 iptables -A INPUT -m state --state INVALID -j DROP
 iptables -A FORWARD -m state --state INVALID -j DROP
 iptables -A OUTPUT -m state --state INVALID -j DROP
-
+#
 # flooding of RST packets, smurf attack Rejection
 iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
-
+#
 # Protecting portscans
 # Attacking IP will be locked for 24 hours X 300 days (3600 x 24 x 300 = 25920000 Seconds)
 iptables -A INPUT -m recent --name portscan --rcheck --seconds 25920000 -j DROP
 iptables -A FORWARD -m recent --name portscan --rcheck --seconds 25920000 -j DROP
-
+#
 # Remove attacking IP after 24 hours
 iptables -A INPUT -m recent --name portscan --remove
 iptables -A FORWARD -m recent --name portscan --remove
-
+#
 # These rules add scanners to the portscan list, and log the attempt.
 iptables -A INPUT -p tcp -m tcp --dport 139 -m recent --name portscan --set -j LOG --log-prefix "portscan:"
 iptables -A INPUT -p tcp -m tcp --dport 139 -m recent --name portscan --set -j DROP
-
+#
 iptables -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j LOG --log-prefix "portscan:"
 iptables -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j DROP
-
+#
 # Allow the following ports through from outside
 iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-
-
-
+#
+#
+#
 # block ping means ICMP port is close (If you do not want ping replace ACCEPT with REJECT)
 iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
 iptables -A OUTPUT -p icmp --icmp-type echo-reply -j DROP
-
+#
 # Lastly DROP All INPUT traffic
 iptables -A INPUT -j DROP
-
-
+#
+#
 ################# Below are for OUTPUT iptables rules #############################################
-
+#
 ## Allow loopback OUTPUT 
 #iptables -A OUTPUT -o lo -j ACCEPT
 #iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
+#
 # Allow the following ports through from outside 
 # SMTP = 25
 # DNS =53
@@ -87,18 +87,19 @@ iptables -A INPUT -j DROP
 # HTTPS = 443
 # SSH = 22
 ### You can also add or remove port no. as per your requirement
-
+#
 #iptables -A OUTPUT -p tcp -m tcp --dport 25 -j ACCEPT
 #iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
 #iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
 #iptables -A OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT
 #iptables -A OUTPUT -p tcp -m tcp --dport 22 -j ACCEPT
-
+#
 # Allow pings
 #iptables -A OUTPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
-
+#
 # Lastly Reject all Output traffic
 #iptables -A OUTPUT -j REJECT
-
+#
 ## Reject Forwarding  traffic
 #iptables -A FORWARD -j REJECT
+#
